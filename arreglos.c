6f51c9affc25676e5f43b43936cfcfdc7e542406
Andradeudla <daniel.andrade.rosillo@udla.edu.ec>
2023-11-30 19:00:08 -0500

    segundo commit

--- arreglos.c
+++ arreglos.c
@@ -13,5 +13,20 @@ int main()
 
     int matriz[f][c];
 
+    for (int i = 1; i < m; i++) // La variable de i se inicializa con 1 porque ya se va a usar una matriz fija al final llena de 1
+    
+    {
+        printf("\n Matriz %d", i); // El primer for para ver cuantas matrices se va a utilizar
+        for (int j = 0; j < f; j++)// Segundo for para ver las filas de las matrices
+        {
+            printf("\n");
+            for (int k = 0; k < c; k++)// tercer for para ver las columnas de las matrices
+            {
+                matriz[j][k] = 0; // Se llena las matrices de 0
+                printf("%d \t", matriz[j][k]);// Se imprime las matrices
+            }
+        }
+    }
+
     return 0;
 }
